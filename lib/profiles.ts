bd2e88319e548df08097be7fc48380e71bd07d7a
pawas goyal <pawas48ram@gmail.com>
2024-01-16 14:17:15 +0530

    Changed the url of photos from online to locally avaiable

--- lib/profiles.ts
+++ lib/profiles.ts
@@ -1,17 +1,25 @@
+import { profile } from "console";
+
 export const profiles = [
   {
-    icon: "https://www.svgrepo.com/show/330828/leetcode.svg",
+    icon: "/logos/leetcode.svg",
     profile: "https://leetcode.com/pawas48ram/",
     name: "Leetcode",
   },
   {
-    icon: "https://www.svgrepo.com/show/330186/codeforces.svg",
+    icon: "/logos/codeforces.svg",
     profile: "https://codeforces.com/profile/warriorji",
     name: "Codeforces",
   },
   {
-    icon: "https://www.svgrepo.com/show/331724/github-code-source.svg",
+    icon: "/logos/github-logo.svg",
     profile: "https://github.com/pawas108ram",
     name: "Github",
   },
+  {
+    icon: '/logos/codechef.svg',
+    profile: 'https://www.codechef.com/users/pawas_g108',
+    name: 'Codechef'
+    
+  }
 ];
--- lib/skills.ts
+++ lib/skills.ts
@@ -4,51 +4,43 @@ export const skills = [
         skills: [
             {
                 name: 'React',
-                icon:'https://camo.githubusercontent.com/27d0b117da00485c56d69aef0fa310a3f8a07abecc8aa15fa38c8b78526c60ac/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f72656163742f72656163742d6f726967696e616c2e737667'
+                icon:'/logos/react.svg'
             },
             {
                 name: 'CSS',
-                icon:'https://camo.githubusercontent.com/2e496d4bfc6f753ddca87b521ce95c88219f77800212ffa6d4401ad368c82170/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f637373332f637373332d6f726967696e616c2e737667'
+                icon:'/logos/css-3.svg'
             },
             {
                 name: 'HTML',
-                icon:'https://camo.githubusercontent.com/da7acacadecf91d6dc02efcd2be086bb6d78ddff19a1b7a0ab2755a6fda8b1e9/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f68746d6c352f68746d6c352d6f726967696e616c2e737667'
+                icon:'/logos/html-document.svg'
             },
             {
                 name: 'Material UI',
-                icon:'https://camo.githubusercontent.com/8fae16ce278f9fbb3f4c48dd32f32fa8507fed6c0004231fb2539d66bc933296/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6d6174657269616c75692f6d6174657269616c75692d6f726967696e616c2e737667'
+                icon:'/logos/material-ui.svg'
             },
             {
                 name: 'Framer Motion',
-                icon:'https://cdn.worldvectorlogo.com/logos/framer-motion.svg'
+                icon:'/logos/framer-motion.svg'
             },
             {
                 name: 'Tailwind CSS',
-                icon:'https://camo.githubusercontent.com/1f0d1e1d113a468a91c2d3592ae56e702c30f85d796441956e20b776f27689c7/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f7461696c77696e646373732f7461696c77696e646373732d6f726967696e616c2d776f72646d61726b2e737667'
+                icon:'/logos/tailwindcss-icon.svg'
             },
             {
                 name: 'Next JS',
-                icon:'https://camo.githubusercontent.com/26a528f097ecb4f4b3987ad74cd3086870e930d85124c2a352dbde9e3cd14cb7/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6e6578746a732f6e6578746a732d6f726967696e616c2e737667'
-            },
-            {
-                name: 'Javascript',
-                icon:'https://camo.githubusercontent.com/442c452cb73752bb1914ce03fce2017056d651a2099696b8594ddf5ccc74825e/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6a6176617363726970742f6a6176617363726970742d6f726967696e616c2e737667'
-            },
-            {
-                name: 'Typescript',
-                icon:'https://camo.githubusercontent.com/aa8b3e6b6fc55ea158e132e1c33ba6aa7fe49706a4e4bd64701af1cf89f514b5/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f747970657363726970742f747970657363726970742d6f726967696e616c2e737667'
+                icon:'/logos/nextjs.svg'
             },
             {
                 name: 'Redux',
-                icon:'https://camo.githubusercontent.com/2b6b50702c658cdfcf440cef1eb88c7e0e5a16ce0eb6ab8bc933da7697c12213/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f72656475782f72656475782d6f726967696e616c2e737667'
+                icon:'/logos/redux-logo.svg'
             },
             {
                 name: 'JQuery',
-                icon:'https://camo.githubusercontent.com/ea00f04e50ce727072c4f6c93d402e6a9fdd59aeb1549d31fe6e86651256007b/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6a71756572792f6a71756572792d6f726967696e616c2e737667'
+                icon:'/logos/jquery.svg'
             },
             {
                 name: 'Bootstrap',
-                icon:'https://www.svgrepo.com/show/305812/bootstrap.svg'
+                icon:'/logos/bootstrap.svg'
             }
         ]
         
@@ -58,37 +50,45 @@ export const skills = [
         skills: [
             {
                 name: 'Node JS',
-                icon:'https://camo.githubusercontent.com/900baefb89e187c8b32cdbb3b440d1502fe8f30a1a335cc5dc5868af0142f8b1/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6e6f64656a732f6e6f64656a732d6f726967696e616c2e737667'
+                icon:'/logos/nodejs-icon.svg'
 
             },
             {
                 name: 'Express JS',
-                icon:'https://camo.githubusercontent.com/40756575fc2fd74b1883ea0cc5c2a49aa7048ab58286f43a121109d69a9ea160/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f657870726573732f657870726573732d6f726967696e616c2e737667'
+                icon:'/logos/express.svg'
             },
             {
                 name: 'Mongo DB',
-                icon:'https://camo.githubusercontent.com/9ebde7ca22ab3f3b4bf92d2743804ab9e581e413a16cdf3626c2092e69967d80/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6d6f6e676f64622f6d6f6e676f64622d6f726967696e616c2e737667'
+                icon:'/logos/mongodb.svg'
             },
             {
                 name: 'PostgreSQL',
-                icon:'https://camo.githubusercontent.com/d536b9cc0c533324368535ece721f5424f28eae3ec0e6f3847408948ecacfce6/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f706f737467726573716c2f706f737467726573716c2d6f726967696e616c2e737667'
+                icon:'/logos/postgresql.svg'
             },
             {
                 name: 'Firebase',
-                icon:'https://camo.githubusercontent.com/5e264dc8fcb694c4183413c371048cfb251b0e8ee84b64a0e8ee76750718ca50/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f66697265626173652f66697265626173652d706c61696e2e737667'
+                icon:'/logos/firebase.svg'
             },
             {
                 name: 'GraphQL',
-                icon:'https://camo.githubusercontent.com/4f06622b58ef8d2b07d366a4296dfa73965f02f2a824563afb0c4cca0665da97/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6772617068716c2f6772617068716c2d706c61696e2e737667'
+                icon:'/logos/graphql-svgrepo-com.svg'
             },
             {
                 name: 'MySQL',
-                icon:'https://camo.githubusercontent.com/2582ec2237a3a1fbd34e9b57332b72be27a7facb32abe7c2335e5f86e5f457a8/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6d7973716c2f6d7973716c2d6f726967696e616c2e737667'
+                icon:'/logos/mysql-logo.svg'
             },
             {
                 name: 'Redis',
-                icon:'https://camo.githubusercontent.com/2af3e6d89e3c22ea6bbde81c96d0d0674bbed3d5e26c572e0b6ad247732c4375/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f72656469732f72656469732d6f726967696e616c2e737667'
+                icon:'/logos/redis-logo.svg'
             },
+            {
+                name: 'Prisma',
+                icon:'/logos/prisma.svg'
+            },
+            {
+                name: 'Django',
+                icon:'/logos/django-icon.svg'
+            }
 
 
 
@@ -99,23 +99,49 @@ export const skills = [
         skills: [
             {
                 name: 'Git',
-                icon:'https://camo.githubusercontent.com/dc9e7e657b4cd5ba7d819d1a9ce61434bd0ddbb94287d7476b186bd783b62279/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6769742f6769742d6f726967696e616c2e737667'
+                icon:'/logos/git-icon-logo.svg'
             },
             {
                 name: 'Github',
-                icon:'https://camo.githubusercontent.com/6c8e86dfc77346d4388b8e064db73017a210f18e2cd18e74779ea34f2d630f4a/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f6769746875622f6769746875622d6f726967696e616c2e737667'
+                icon:'/logos/github-logo.svg'
             },
             {
                 name: 'POSTMAN',
-                icon:'https://www.svgrepo.com/show/354201/postman.svg'
+                icon:'/logos/postman.svg'
             },
             {
                 name: 'SASS',
-                icon:'https://camo.githubusercontent.com/587d0f411b348ee05a53c7685b59142e0705ff8d06181d09008438c1a92f1a96/68747470733a2f2f7261776769742e636f6d2f736173732f736173732d736974652f6d61696e2f736f757263652f6173736574732f696d672f6c6f676f732f6c6f676f2e737667'
+                icon:'/logos/sass.svg'
             },
             {
                 name: 'Vercel',
-                icon:'https://www.svgrepo.com/show/327408/logo-vercel.svg'
+                icon:'/logos/logo-vercel.svg'
+            },
+            {
+                name: 'Docker',
+                icon:'/logos/docker-svgrepo-com.svg'
+            }
+        ]
+    },
+    {
+        skillDomain: 'Programming Languages',
+        skills: [
+            {
+                name: 'C++',
+                icon: '/logos/c-plus-plus.svg'
+            
+            },
+            {
+                name: 'Javascript',
+                icon:'/logos/javascript-logo.svg'
+            },
+            {
+                name: 'Typescript',
+                icon:'/logos/typescript-icon.svg'
+            },
+            {
+                name: 'Python',
+                icon:'/logos/python.svg'
             }
         ]
     }
