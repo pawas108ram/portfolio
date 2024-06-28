28bfea7d05a66ec208dbbd27f30c01f8ce9f868c
Pawas Goyal <pawas48ram@gmail.com>
2024-06-28 14:38:00 +0530

    Changed the flex col to flex row

--- app/components/Contacts.tsx
+++ app/components/Contacts.tsx
@@ -4,20 +4,22 @@ import React from 'react'
 
 const Contacts = () => {
   return (
-      <div className='bg-black/40 text-white rounded w-full flex flex-row p-4 gap-2 z-99999 '>
+      <div className='bg-black/40 text-white rounded w-full flex flex-col p-4 gap-2 z-99999 '>
           <span className='underline lg:text-4xl xs:text-2xl '>Socials</span>
-          <span className='flex flex-row items-center gap-4 lg:text-lg  xs:text-sm'>
-             <Image src='https://www.svgrepo.com/show/303161/gmail-icon-logo.svg' alt='gmail' height={32} width={32} />
-              <Link href='mailto:pawas48ram@gmail.com' className='text-blue-500 underline'>Gmail</Link>
-          </span>
-          <span className='flex flex-row items-center gap-4 lg:text-lg xs:text-sm'>
-              <Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png' alt='linkedin' height={32} width={32} />
-              <Link href='https://www.linkedin.com/in/pawas-goyal/' className='text-blue-500 underline'>LinkedIn</Link>
-          </span>
-          <span className='flex flex-row items-center gap-4 lg:text-lg  xs:text-sm'>
-              <Image src='https://www.sarkarinaukriexams.com/images/import/sne13333514974.png' alt='twitter' height={32} width={32}  />
-              <Link href='https://twitter.com/Weeb_NotMeCoder' className='text-blue-500 underline'>Twitter</Link>
-          </span>
+          <div className='flex flex-row gap-2 items-center p-2'>
+              <span className='flex flex-row items-center gap-4 lg:text-lg  xs:text-sm'>
+                 <Image src='https://www.svgrepo.com/show/303161/gmail-icon-logo.svg' alt='gmail' height={32} width={32} />
+                  <Link href='mailto:pawas48ram@gmail.com' className='text-blue-500 underline'>Gmail</Link>
+              </span>
+              <span className='flex flex-row items-center gap-4 lg:text-lg xs:text-sm'>
+                  <Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png' alt='linkedin' height={32} width={32} />
+                  <Link href='https://www.linkedin.com/in/pawas-goyal/' className='text-blue-500 underline'>LinkedIn</Link>
+              </span>
+              <span className='flex flex-row items-center gap-4 lg:text-lg  xs:text-sm'>
+                  <Image src='https://www.sarkarinaukriexams.com/images/import/sne13333514974.png' alt='twitter' height={32} width={32}  />
+                  <Link href='https://twitter.com/Weeb_NotMeCoder' className='text-blue-500 underline'>Twitter</Link>
+              </span>
+          </div>
       
     </div>
   )
